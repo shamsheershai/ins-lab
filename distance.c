561766c049e579580721708d103fc43e78b8fa43
shamsheershai <35594269+shamsheershai@users.noreply.github.com>
2019-02-25 11:53:25 +0530

    Add files via upload

---
+++ distance.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#define MAX 10
+int main()
+{
+  struct rtable
+{
+ unsigned distance[MAX];
+unsigned from[MAX];
+}rt[10];
+int dmatrix[MAX][MAX];
+int n=0,i=0,j=0,k=0,count=0;
+printf("enter number of node");
+scanf("%d",&n);
+printf("enter the matrix 0 for self loop and 999 for no path");
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+scanf("%d",&dmatrix[i][j]);
+if(i==j)
+dmatrix[i][j]=0;
+}
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+rt[i].distance[j]=dmatrix[i][j];
+rt[i].from[j]=j;
+}
+}
+do
+{
+count=0;
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+for(k=0;k<n;k++)
+{
+if(rt[i].distance[j]>dmatrix[i][k]+rt[k].distance[j])
+{
+rt[i].distance[j]=rt[i].distance[k]+rt[k].distance[j];
+rt[i].from[j]=k;
+count++;
+}
+}
+}
+}
+}while(count!=0);
+for(i=0;i<n;i++)
+{
+printf("the start value for router %d\n",i);
+for(j=0;j<n;j++)
+printf("from node%d via %d dist %d\n",j,rt[i].from[j],rt[i].distance[j]);
+}
+return 0;
+}
